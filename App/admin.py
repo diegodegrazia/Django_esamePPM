da845054543efa52e8f09ff74441cd6813fedbcb
diegodegrazia <diego.degrazia@stud.unifi.it>
2024-06-13 18:50:26 +0200

    Customized django administration models

--- App/admin.py
+++ App/admin.py
@@ -1,14 +1,51 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 from .models import Post, Comment, Blog
 
 
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'author', 'created_on', 'updated_on')
+    list_filter = ('author', 'created_on',)
+    search_fields = ['name', 'slug']
+    fields = ['name', 'author']
+
+
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'created_on')
-    list_filter = ('status',)
-    search_fields = ['status', 'content']
+    list_display = ('title', 'slug', 'created_on', 'updated_on')
+    list_filter = ('status', 'created_on',)
+    search_fields = ['title', 'slug', 'content']
+    fields = ['title', 'author', 'blog', 'content']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'post', 'author', 'created_on', 'updated_on')
+    list_filter = ('author', 'post', 'created_on',)
+    search_fields = ['id', 'content']
+    fields = ['author', 'post', 'content']
+
+
+class CustomUserAdmin(UserAdmin):
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Informazioni personali', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permessi', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('username', 'first_name', 'last_name')
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
 
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Comment)
-admin.site.register(Blog)
-
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Blog, BlogAdmin)
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
